2f130eefd2456ffd69c8861f1faab0b769f0101b
iris310604 <3106047057@qq.com>
2026-02-03 15:11:10 +0800

    Update 腾讯.py
    关于腾讯视频的py脚本，采用监听，优点代码短，缺点需要打开浏览器，后续加入线程池加速

--- update.py/腾讯.py
+++ update.py/腾讯.py
@@ -14,11 +14,13 @@ def get_from_args():
         print('未输入网址')
         return None
 
+#打开浏览器监听特定接口
 dp = ChromiumPage()
 dp.listen.start('proxyhttp')
 a = get_from_args()
 dp.get(a)
 resp = dp.listen.wait()
+#获取文本以及视频链接
 json_data = resp.response.body
 v_info = json.loads(json_data['vinfo'])
 m3u8_url = v_info['vl']['vi'][0]['ul']['ui'][-1]['url']
@@ -26,9 +28,12 @@ title = v_info['vl']['vi'][0]['ti']
 m3u8 = requests.get(m3u8_url).text
 ts_list = re.findall(',\n(.*?)\n#', m3u8)
 ts_name = '/'.join(m3u8_url.split('/')[:-1]) + '/'
+#确保保存文件夹存在
 os.makedirs('视频', exist_ok=True)
+#遍历每个视频链接将其追加写入
 for ts in tqdm(ts_list):
     ts_url = ts_name + ts
     ts_content = requests.get(ts_url).content
     with open('视频\\' + title + '.mp4', 'ab') as f:
         f.write(ts_content)
+
